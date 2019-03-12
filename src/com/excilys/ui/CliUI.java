45e5101e0184115e2d3f020ce2c40fa62b18b53a
leocharlier <lcharlier@excilys.com>
2019-03-12 14:09:33 +0100

    Lists, Show, Create and Delete Computer

--- src/com/excilys/ui/CliUI.java
+++ src/com/excilys/ui/CliUI.java
@@ -1,6 +1,9 @@
 package com.excilys.ui;
 
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 import com.excilys.model.Company;
 import com.excilys.model.Computer;
@@ -13,6 +16,27 @@ public class CliUI {
 		DAOFactory daoFactory = DAOFactory.getInstance();	
 		CompanyDAO companyDAO = daoFactory.getCompanyDao();
 		ComputerDAO computerDAO = daoFactory.getComputerDao();
+		
+		Computer computer = new Computer();
+		computer.setName("Test");
+		
+//		try {
+//			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss.SSS");
+//			Date parsedDate = dateFormat.parse("2018-05-21 21:05:21.000");
+//			Timestamp introduced = new Timestamp(parsedDate.getTime());
+//		} catch (Exception e) {
+//		
+//		}
+		
+		computer.setIntroduced(null);
+		computer.setDiscontinued(null);
+		computer.setCompany(companyDAO.find(2));
+		
+		computerDAO.create(computer);
+		
+		System.out.println("Computer added : " + computer.getId());
+		
+		computerDAO.delete(computer);
 		
 //		ArrayList<Company> companies = companyDAO.list();
 //		ArrayList<Computer> computers = computerDAO.list();
