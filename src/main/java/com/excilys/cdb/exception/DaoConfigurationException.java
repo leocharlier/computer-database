54ae6359371d84dc3b75d5f497e09a385fc82272
leocharlier <lcharlier@excilys.com>
2019-03-27 11:41:03 +0100

    HikariCP integration

--- src/main/java/com/excilys/cdb/exception/DaoConfigurationException.java
+++ src/main/java/com/excilys/cdb/exception/DaoConfigurationException.java
@@ -4,11 +4,6 @@ import org.apache.log4j.Logger;
 
 public class DaoConfigurationException extends RuntimeException {
   static final Logger LOGGER = Logger.getLogger(DaoConfigurationException.class);
-  
-  public DaoConfigurationException(String message) {
-    super(message);
-    LOGGER.error(message);
-  }
 
   public DaoConfigurationException(String message, Throwable cause) {
     super(message, cause);
--- src/main/java/com/excilys/cdb/persistence/DaoFactory.java
+++ src/main/java/com/excilys/cdb/persistence/DaoFactory.java
@@ -1,69 +1,39 @@
 package com.excilys.cdb.persistence;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 
 import com.excilys.cdb.exception.DaoConfigurationException;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 public class DaoFactory {
-
-  private static final String FICHIER_PROPERTIES = "properties/dao.properties";
-  private static final String PROPERTY_URL = "url";
-  private static final String PROPERTY_DRIVER = "driver";
-  private static final String PROPERTY_USER = "user";
-  private static final String PROPERTY_PWD = "pwd";
-
-  private String url;
-  private String username;
-  private String password;
-
-  DaoFactory(String url, String username, String password) {
-    this.url = url;
-    this.username = username;
-    this.password = password;
+	private static final String FICHIER_PROPERTIES = "/properties/hikari.properties";
+  private HikariDataSource dataSource;
+  private static DaoFactory INSTANCE = null;
+  
+  private DaoFactory(HikariDataSource ds){
+	  this.dataSource = ds;
   }
 
   public static DaoFactory getInstance() throws DaoConfigurationException {
-    Properties properties = new Properties();
-    String url;
-    String driver;
-    String user;
-    String pwd;
-
-    ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-    InputStream fichierProperties = classLoader.getResourceAsStream(FICHIER_PROPERTIES);
-
-    if (fichierProperties == null) {
-      throw new DaoConfigurationException("The file " + FICHIER_PROPERTIES + " is unreachable.");
-    }
-
-    try {
-      properties.load(fichierProperties);
-      url = properties.getProperty(PROPERTY_URL);
-      driver = properties.getProperty(PROPERTY_DRIVER);
-      user = properties.getProperty(PROPERTY_USER);
-      pwd = properties.getProperty(PROPERTY_PWD);
-    } catch (IOException e) {
-      throw new DaoConfigurationException("Impossible to load the property file "
-         + FICHIER_PROPERTIES, e);
-    }
-
-    try {
-      Class.forName(driver);
-    } catch (ClassNotFoundException e) {
-      throw new DaoConfigurationException("The driver is unreachable in the classpath.", e);
-    }
-
-    DaoFactory instance = new DaoFactory(url, user, pwd);
-    return instance;
+	  if(INSTANCE == null) {
+		  try {
+			  HikariConfig config = new HikariConfig(FICHIER_PROPERTIES);
+			  HikariDataSource dataSource = new HikariDataSource(config);
+			  INSTANCE = new DaoFactory(dataSource);
+		  } catch(IllegalArgumentException e) {
+			  throw new DaoConfigurationException("Impossible to load the property file " + FICHIER_PROPERTIES, e);
+		  } catch(RuntimeException e) {
+			  throw new DaoConfigurationException("Property file issue in " + FICHIER_PROPERTIES, e);
+		  }
+	  }
+	  
+	  return INSTANCE;
   }
 
   protected Connection getConnection() throws SQLException {
-    return DriverManager.getConnection(url, username, password);
+	  return dataSource.getConnection();
   }
 
   public CompanyDao getCompanyDao() {
